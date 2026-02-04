b9f20783fba5895b3d3f2fe26772d6f661a6ce8d
root <root@DESKTOP-NND50N7.localdomain>
2026-02-04 12:06:54 -0500

    Added code to server.js and nodemon dev dependency

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3000;
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
